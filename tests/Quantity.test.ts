9727c69d6a852842406fd09ad54278ffa2661100
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-11-09 12:13:26 +0200

    chore: bump combine version

--- tests/Quantity.test.ts
+++ tests/Quantity.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 import { Duckling, Quantity } from "../mod.ts";
 
 Deno.test("Quantity", () => {
--- tests/Temperature.test.ts
+++ tests/Temperature.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 import { Duckling } from "../mod.ts";
 
 Deno.test("fahrneheit", () => {
